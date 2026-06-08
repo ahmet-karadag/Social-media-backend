09a4405230c8fa4b3ed0aecb13251a2462d5bfab
Ahmet Karadağ <akkaradaga@gmail.com>
2026-06-08 21:52:01 +0300

    feat: complete comment routes and integrate into app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import authRoutes from './routes/auth';
 import userRoutes from './routes/user';
 import messageRoutes from './routes/message';
 import postRoutes from './routes/post';
+import commentRoutes from './routes/comment';
 
 dotenv.config();
 connectDB();
@@ -20,6 +21,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/posts',postRoutes);
+app.use('/api/comments',commentRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/routes/comment.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const router:Router = Router();
+
+import authMiddleware from "../middleware/auth";
+import *as commentController from '../controllers/comment' //*as hepsini tek obje altında tutmak için,tek tek export ettiğim için
+
+router.post('/create',authMiddleware, commentController.createComment);
+router.get('/:postId',authMiddleware,commentController.getCommentsByPost);
+
+export default router;
